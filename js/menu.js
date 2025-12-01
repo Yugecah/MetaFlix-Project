943e944c378549cfd22365b47523f9ea44974be1
cabantopj-cmd <cabantopj@gmail.com>
2025-12-01 23:09:00 +0800

    Delete js/menu.js

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const nav = document.querySelector(".nav");
-
-toggleBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});

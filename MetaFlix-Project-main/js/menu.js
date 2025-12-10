487fc424e566933ad378a27d977b869f09a73721
Isayy01 <kisha.caballero@gmail.com>
2025-12-10 17:39:58 +0400

    Add files via upload

---
+++ MetaFlix-Project-main/js/menu.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav");
+
+toggleBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
